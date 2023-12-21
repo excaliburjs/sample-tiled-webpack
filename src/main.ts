e4c23a7ce73c2cd215219939f7da19a5e0cf592f
Erik Onarheim <erik.onarheim@gmail.com>
2023-12-21 10:02:41 -0600

    Update working sample

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import * as ex from 'excalibur';
 import { Resources, loader } from './resources';
 import { Player } from './player';
+import { DevTool } from '@excaliburjs/dev-tools';
+import { TiledObjectComponent } from '@excaliburjs/plugin-tiled';
 
 const game = new ex.Engine({
     width: 800,
@@ -11,17 +13,42 @@ const game = new ex.Engine({
 
 game.start(loader).then(() => {
     const objects = Resources.TiledMap.data.getObjectLayerByName("Objects");
-    const camera = objects.getObjectByName("Camera");
-    if (camera) {
-        game.currentScene.camera.pos = ex.vec(camera.x, camera.y);
-        game.currentScene.camera.zoom = camera.getProperty<number>('zoom')?.value ?? 1.0;
-    }
+    const script = Resources.TiledMap.data.getObjectLayerByName("script");
 
+    for (let obj of script.objects) {
+        // Cannot rely on [0] so need to loop for value = portal and name = name?
+        if (obj.properties.some(p => p.value ==='portal')){ // < -- no longer depends on prop order
+            // Actors have a built in circle collider if radius is set
+            const actorWithCircleCollider = new ex.Actor({
+                pos: ex.vec(obj.x + obj.width!/2, obj.y + obj.width!/2), // < -- tiled renders stuff oddly so this is a workaround
+                radius: (obj.width || 20) / 2, // < - radius is 1/2 width
+                collisionType: ex.CollisionType.Passive,
+                color: ex.Color.Green, // <-- debug color
+                z: 99,  // < -- debug z above all
+                name:'portal',
+                anchor: ex.vec(0.5, 0.5), // < -- switch back to center
+            });
+
+            actorWithCircleCollider.on('collisionstart', () => {
+                console.log('Look at the portal on that!');
+            });
+            game.currentScene.add(actorWithCircleCollider);
+        }
+    }
     const player = objects.getObjectByName("Player");
     if (player) {
         const playerActor = new Player(ex.vec(player.x, player.y));
+        playerActor.on('collisionstart', evt => {
+            const data = evt.other.get(TiledObjectComponent)
+            console.log(data);
+        });
+        console.log(player.x, player.y);
         game.currentScene.add(playerActor);
         playerActor.z = 100;
+        playerActor.scale = new ex.Vector(2, 2);
+        game.currentScene.camera.strategy.elasticToActor(playerActor, .8, .9);
     }
+    const devtool = new DevTool(game);
+
     Resources.TiledMap.addTiledMapToScene(game.currentScene);
 });
--- src/resources.ts
+++ src/resources.ts
@@ -4,7 +4,7 @@ import { TiledMapResource } from '@excaliburjs/plugin-tiled';
 
 export const Resources = {
     HeroSpriteSheetPng: new ImageSource('./img/Solaria Demo Pack Update 03/Solaria Demo Pack Update 03/16x16/Sprites/Hero 01.png', false, ImageFiltering.Pixel),
-    TiledMap: new TiledMapResource('./res/first-level.tmx')
+    TiledMap: new TiledMapResource('./res/beginner_town.tmx')
 }
 
 // Change the path to be relative to the root directory for the webpack prod build
