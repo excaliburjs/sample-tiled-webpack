db3b4f0ce336541557c2ccc3232ee76740718749
eonarheim <erik.onarheim@gmail.com>
2024-12-26 01:05:00 +0000

    Deploy Tiled Webpack

--- res/tileset.tsx
+++ res/tileset.tsx
@@ -1,9 +1,14 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="Base" tilewidth="16" tileheight="16" tilecount="420" columns="28">
+<tileset version="1.10" tiledversion="1.10.2" name="Base" tilewidth="16" tileheight="16" tilecount="420" columns="28">
  <image source="../img/Solaria Demo Pack Update 03/Solaria Demo Pack Update 03/16x16/Tilesets/Solaria Demo Update 01.png" width="448" height="240"/>
  <tile id="61">
   <objectgroup draworder="index" id="2">
    <object id="1" x="2" y="1" width="13" height="13"/>
+  </objectgroup>
+ </tile>
+ <tile id="230">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
 </tileset>
